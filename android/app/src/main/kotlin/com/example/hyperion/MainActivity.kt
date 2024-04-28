a096d808953d8fe5e0a9e92e28a9aaa15e80f7ac
pakpoomcs <pakpoom.cs@gmail.com>
2024-04-28 17:32:09 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/hyperion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hyperion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(const Hyperion());
+}
+
+class Hyperion extends StatelessWidget {
+  const Hyperion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.green[100],
+          body: const Center(child: Text("Hyperion Body")),
+        ));
+  }
+}
